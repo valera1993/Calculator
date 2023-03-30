aaf48131e9b68144902da54fc39956f44a2f413c
Julia <markiza1014@gmail.com>
2023-03-30 15:44:07 +0300

    Chenges Class and add function

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
 let firstNumb = document.querySelector("#firstNumber");
-let secondNumber = document.querySelector("#secondNumber");
+let secondNumb = document.querySelector("#secondNumber");
 let addition = document.querySelector("#addition");
 let subtraction = document.querySelector("#subtraction");
 let multiplication = document.querySelector("#multiplication");
 let division = document.querySelector("#division");
 
-
-class Calculator{
+class Calculator {
     firstValue;
     secondValue;
-    construcor(firstValue, secondValue){
-        firstValue = this.firstValue,
-        secondValue = this.secondValue
+    constructor(firstValue, secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
     }
-    additionNumbs(){
-
+    additionNumbs() {
         return alert(this.firstValue + this.secondValue);
     }
-    subtraction(){
-        return this.firstValue - this.secondValue;
+    subtraction() {
+        return alert(this.firstValue - this.secondValue);
     }
-    multiplication(){
-        return this.firstValue * this.secondValue;
+    multiplication() {
+        return alert(this.firstValue * this.secondValue);
     }
-    division(){
-        return this.firstValue / this.secondValue;
+    division() {
+        return alert(this.firstValue / this.secondValue);
     }
 }
 
-let res = new Calculator(Number(firstNumb.value), Number(secondNumber.value));
+function identif(sign) {
+    let res = new Calculator(Number(firstNumb.value), Number(secondNumb.value));
+    switch (sign) {
+        case addition:
+            res.additionNumbs();
+            firstNumb.value = '';
+            secondNumb.value = '';
+            break;
+        case subtraction:
+            res.subtraction();
+            firstNumb.value = '';
+            secondNumb.value = '';
+            break;
+        case multiplication:
+            res.multiplication();
+            firstNumb.value = '';
+            secondNumb.value = '';
+            break;
+        case division:
+            res.division();
+            firstNumb.value = '';
+            secondNumb.value = '';
+            break;
+    }
+}
 
-addition.addEventListener("click", res.additionNumbs)
-subtraction.addEventListener("click", function(){
-    alert(firstNumb.value)
-})
+addition.addEventListener("click", () => { identif(addition) });
+subtraction.addEventListener("click", () => { identif(subtraction) });
+multiplication.addEventListener("click", () => { identif(multiplication) });
+division.addEventListener("click", () => { identif(division) });
